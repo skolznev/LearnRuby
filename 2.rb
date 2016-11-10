ec6f9765936c4735d105fae861ce181cdcac009c
Dmitry Skolznev <skolznevdm@gmail.com>
2016-11-10 07:38:21 +0300

    LR_10.11.2016

--- 2.rb
+++ 2.rb
@@ -3,4 +3,16 @@
 # cls - очистка командной строки
 # irb - оболочка руби
 
-v1, time = 0.31.00
+#v1, time = 0.36.29
+
+#Ctrl + B - вычисление в Sublime
+
+class Fixnum
+
+	def + value
+		self - value
+	end
+
+end
+
+p 1 + 6
