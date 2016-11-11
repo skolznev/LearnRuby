2ca12abdd1aced523c961e3ffa87955fd51e936a
Dmitry Skolznev <skolznevdm@gmail.com>
2016-11-11 07:31:10 +0300

    LR_11.11.2016

--- 2.rb
+++ 2.rb
@@ -3,16 +3,34 @@
 # cls - очистка командной строки
 # irb - оболочка руби
 
-#v1, time = 0.36.29
+#v1, time = 1.04.04
 
 #Ctrl + B - вычисление в Sublime
+# = - присвоение
+# == - сравнение
+# === - принадлежность
 
-class Fixnum
+#class Fixnum
 
-	def + value
-		self - value
-	end
+#	def + value
+#		self - value
+#	end
 
+#end
+
+#p 1 + 6
+
+user = {
+	name: 'Spider-man',
+	role: :superhero
+}
+
+if user[:role] == :superhero
+	puts user[:name] + ' please, help us!'
+else
+	puts "Let's hide in the wood"
 end
 
-p 1 + 6
+
+
+
